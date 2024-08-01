947ee2925398d84f4fc3fe434693abb430aade55
marco-soria <marco.soriaa7@gmail.com>
2024-08-01 09:34:23 -0500

    seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,23 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import vue from "@astrojs/vue";
-
 import svelte from "@astrojs/svelte";
+import EventEmitter from 'events';
+import sitemap from "@astrojs/sitemap";
+EventEmitter.defaultMaxListeners = 20;
+
+// Capturar y mostrar warnings
+process.on('warning', (warning) => {
+  console.warn(warning.name);    // Ejemplo: MaxListenersExceededWarning
+  console.warn(warning.message); // Ejemplo: Possible EventEmitter memory leak detected.
+  console.warn(warning.stack);   // Stack trace
+});
+
+
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), react(), vue(), svelte()]
+  site: 'https://astrobuild',
+  integrations: [tailwind(), mdx(), react(), vue(), svelte(), sitemap()]
 });
